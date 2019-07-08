85e5eb2664bb11fe76677a4b3b4d6fc07bc0831c
hjzzju <595445628@qq.com>
2019-07-08 22:51:20 +0800

    hjz next

--- app/src/main/java/com/hjz/helloworld/MainActivity.java
+++ app/src/main/java/com/hjz/helloworld/MainActivity.java
@@ -3,12 +3,28 @@ package com.hjz.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText edittext;
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edittext = (EditText)findViewById(R.id.EditText);
+        button = (Button)findViewById(R.id.Button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Log.d("Log.D", "onClick: Button");
+                edittext.setText("This Button");
+            }
+        });
     }
 }
